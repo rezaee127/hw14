8249e5551b2c76087a66bed848b309b790c3430d
razi <rezaie127@yahoo.com>
2022-04-28 14:59:37 +0430

    added data binding

--- app/src/main/java/ui/WordAdapter.kt
+++ app/src/main/java/ui/WordAdapter.kt
@@ -10,19 +10,18 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.hw14.R
+import com.example.hw14.databinding.WordRowItemBinding
 import model.Word
 
 class WordAdapter(var onClickItem:(Int)->Unit) : ListAdapter<Word, WordAdapter.ViewHolder>(WordDiffCallback) {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val textViewWord=view.findViewById<TextView>(R.id.textViewWord)
-        val textViewMeaning=view.findViewById<TextView>(R.id.textViewMeaning)
-        val wordRowItems=view.findViewById<View>(R.id.wordRowItems)
+    class ViewHolder(val binding: WordRowItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
 
         fun bind(word:Word,onClickItem: (Int) -> Unit){
-            textViewWord.text=word.word
-            textViewMeaning.text=word.Meaning
-            wordRowItems.setOnClickListener {
+            binding.word=word
+
+            binding.wordRowItems.setOnClickListener {
                 onClickItem(word.id)
             }
         }
@@ -31,10 +30,10 @@ class WordAdapter(var onClickItem:(Int)->Unit) : ListAdapter<Word, WordAdapter.V
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
 
-        val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.word_row_item, viewGroup, false)
+        val inflater = LayoutInflater.from(viewGroup.context)
+        val binding= WordRowItemBinding.inflate(inflater)
+        return ViewHolder(binding)
 
-        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
